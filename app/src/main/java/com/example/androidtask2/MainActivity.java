d6e01435297b0838f97d2da11a7f766652e7a654
hadeer <hadeeressam21@gmail.com>
2024-07-20 00:30:30 +0300

    create RecyclerView for courses tabs, and creating the component style, ADAPTER, ViewHolder, and bind all together in main screen (using JAVA).

--- app/src/main/java/com/example/androidtask2/MainActivity.java
+++ app/src/main/java/com/example/androidtask2/MainActivity.java
@@ -7,14 +7,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView CoursesRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        CoursesRecyclerView = findViewById(R.id.tabs_recyclerView);
+
+        LinearLayoutManager layoutManger = new LinearLayoutManager(this , LinearLayoutManager.VERTICAL, false);
+
+        CoursesRecyclerView.setLayoutManager(layoutManger);
+
+        RecyclerView.Adapter adaptor = new CourseTabsAdaptor();
+        CoursesRecyclerView.setAdapter(adaptor);
 
     }
 }
