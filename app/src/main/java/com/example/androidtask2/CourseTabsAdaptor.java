813eb677f555dd853879c4d84f1431fe97af5002
hadeer <hadeeressam21@gmail.com>
2024-07-20 21:45:56 +0300

    create onclick listener for course tab button, to navigate for details screen, make it navigate with data (item index) using intent putExtra, to dispatch the curse details data in the next screen by index.

--- app/src/main/java/com/example/androidtask2/CourseTabsAdaptor.java
+++ app/src/main/java/com/example/androidtask2/CourseTabsAdaptor.java
@@ -1,17 +1,22 @@
 package com.example.androidtask2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.androidtask2.Data.CourseDetailsData;
 import com.example.androidtask2.Data.TabContent;
 
 import java.util.ArrayList;
@@ -40,8 +45,24 @@ public class CourseTabsAdaptor extends RecyclerView.Adapter<CourseTabsAdaptor.Vi
 
         TabContent courseData = TabContent.getCoursesTabData().get(position);
 
+        Context context = holder.itemView.getContext();
+
         holder.courseImage.setImageResource(courseData.image);
         holder.courseText.setText(courseData.courseText);
+
+        holder.courseText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View itemView) {
+                if(itemView instanceof Button){
+                    Toast.makeText(itemView.getContext(),  "Hello " , Toast.LENGTH_LONG).show();
+
+                    Intent intent = new Intent( context, CourseDetailsActivity.class);
+                    int index = courseData.id -1 ;
+                    intent.putExtra("content", index);
+                    context.startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
@@ -51,7 +72,7 @@ public class CourseTabsAdaptor extends RecyclerView.Adapter<CourseTabsAdaptor.Vi
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public ImageView courseImage;
-        public TextView courseText;
+        public Button courseText;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
